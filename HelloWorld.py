11af3df38e312ff7d55e92c0617ea1b020365ecf
Rajiv Kumar - S <Rajiv.1.Kumar@coforge.com>
2025-02-12 15:05:58 +0530

    python learning

---
+++ HelloWorld.py
@@ -0,0 +1,33 @@
+print("Hello World")
+print("-"*10)
+price=10;
+print(price)
+rating=9.5
+name="hello"
+print(rating)
+print(name)
+is_published=False;
+print(is_published)
+
+employee_name="Virat";
+employee_age=20;
+is_new_employee=True;
+#your_name = input('enter your name')
+#favourite_color=input('enter your favourite color')
+#print(your_name +' likes '+favourite_color +' color')
+
+#birth_year = input('Birth Year :')
+#age = 2025 - int(birth_year)
+#print(age)
+#print(type(age))
+course= "Python course for beginners"
+print(course)
+print(course[0])
+print(course[-1])
+print(course[0:5])
+print(course[0:])
+print(course[1:])
+
+#copy of string
+another=course[:] 
+print(another)
