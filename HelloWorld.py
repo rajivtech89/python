804a747ed6c23ab6c3f796f41defdf2ac9526026
Rajiv Kumar - S <Rajiv.1.Kumar@coforge.com>
2025-02-17 18:13:08 +0530

    string operation and logical operator

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,4 @@
+"""
 print("Hello World")
 print("-"*10)
 price=10;
@@ -31,3 +32,62 @@ print(course[1:])
 #copy of string
 another=course[:] 
 print(another)
+print(course[1:-1])
+name = "Alice"
+age = 30
+formatted_string = f"My name is {name} and I am {age} years old."
+print(formatted_string)
+
+#string methods
+course = "Python course for beginners"
+print(len(course))
+print(course.upper()) # this does not modify the original string
+print(course)
+print(course.find('P')) # returns the index of the first occurrence of the character
+print(course.replace('beginners', 'absolute beginners'))
+print('Python' in course)   # returns a boolean value 
+a=10;
+a+=5;
+print(a)
+
+#operator precedence
+x=10+3*2
+print(x)
+
+x=2.9
+print(round(x))
+
+"""
+import math
+print(math.ceil(2.9))
+print(math.floor(2.9))  
+
+is_hot=True
+is_cold=False
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water") 
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print ("It's a lovely day")
+print("Enjoy your day")
+
+price=1000000
+has_good_credit=True
+if has_good_credit:
+    down_payment=0.1*price
+else:
+    down_payment=0.2*price
+print(f"Down payment: ${down_payment}")
+
+
+
+#logical operators
+if has_good_credit and price>100000:
+    print("Eligible for loan")
+has_good_credit=False
+has_high_income=True
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
